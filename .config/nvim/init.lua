38c7a98f1e516bfdbacef2bc093ce3edba999e7a
Luis M Canaval S <public02@canaval.org>
2021-07-26 23:40:51 -0500

    right align line count on folded groups

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,19 +19,19 @@ opt.completeopt  = {"menuone", "noselect"}
 opt.cursorcolumn = true
 opt.cursorline   = true
 opt.expandtab    = false
-opt.fillchars    = {fold=" ", stl="-", stlnc="-"}
-opt.laststatus   = 0
+opt.fillchars    = {fold=" ", vert="╳"}
+opt.laststatus   = 2
 opt.list         = true
 opt.listchars    = {tab="┼─", trail="∙", extends="»", precedes="«", nbsp="§"}
 opt.mouse        = "a"
-opt.number       = true
+-- opt.number       = true
 opt.scrolloff    = 5
 opt.shiftwidth   = 2
 opt.showbreak    = "▼ "
 opt.showmatch    = true
 opt.smartindent  = true
 opt.softtabstop  = 2
-opt.statusline   = "%t%M%=%v" --%{&ff},%{&fenc?&fenc:&enc}
+opt.statusline   = "%t%M%=%l,%v" --%{&ff},%{&fenc?&fenc:&enc}
 opt.tabstop      = 2
 opt.title        = true
 opt.wildignore   = "*~,*.o,*.tmp"
@@ -51,6 +51,21 @@ local mappings = {
 for m, c in pairs(mappings) do
 	map("n", "<leader>"..m, ":"..c.."<cr>", {noremap = true})
 end
+
+_G.myFoldText = function()
+	local lineCount = (vim.v.foldend - vim.v.foldstart + 1).." lines"
+	local line = vim.fn.getline(vim.v.foldstart)
+	local llen = string.len(line)
+	local lclen = string.len(lineCount)
+	local width = 80 -- TODO min(80, window width)
+	if llen < width - lclen then
+		line = line..string.rep("·", width - llen - lclen)
+	else
+		line = string.sub(line, 1, width - lclen).."…"
+	end
+	return line..lineCount
+end
+opt.foldtext = 'v:lua.myFoldText()'
 
 cmd'colorscheme lmcs'
 cmd'autocmd TermOpen * setlocal nonumber foldcolumn=0'
