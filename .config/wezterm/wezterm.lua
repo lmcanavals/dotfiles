f02973daee54e0527e7e58c14d7431a195b59466
Luis M Canaval S <public02@canaval.org>
2023-07-07 13:48:21 -0500

    new colors alright

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Jason Wryan (terminal.sexy)'
+config.color_scheme = 'BlulocoDark'
 config.enable_tab_bar = false
 config.initial_rows = 30
 config.initial_cols = 130
