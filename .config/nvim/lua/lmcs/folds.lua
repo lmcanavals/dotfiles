9e7d27b77ba21f7f9d35d1e8de3286e8035d887d
Luis M Canaval S <public02@canaval.org>
2023-07-16 00:04:30 -0500

    indentations

--- .config/nvim/lua/lmcs/folds.lua
+++ .config/nvim/lua/lmcs/folds.lua
@@ -1,14 +1,17 @@
 -- author:  lmcanavals
 -- date:    2023-07-14
--- updated: 2023-07-14
+-- updated: 2023-07-15
 
 if vim.g.debug then print "lmcsnvim.folds" end
 
-_G.myFoldText    = function()
-	local lineCount = (vim.v.foldend - vim.v.foldstart + 1) .. " lines"
-	local indents = vim.fn.indents(vim.v.foldstart)
-	local line = string.gsub(vim.fn.getline(vim.v.foldstart), "^%s+", "")
-	line = string.rep("x", indents) .. line
+local v = vim.v
+local fn = vim.fn
+
+function LmcsFoldText()
+	local lineCount = (v.foldend - v.foldstart + 1) .. " lines"
+	local indent = fn.indent(v.foldstart)
+	local newindent = string.rep(" ", indent)
+	local line = string.gsub(fn.getline(v.foldstart), "^%s+", newindent)
 	local llen = string.len(line)
 	local lclen = string.len(lineCount)
 	local width = 80 -- TODO min(80, window width)
@@ -20,6 +23,6 @@ _G.myFoldText    = function()
 	end
 	return line .. lineCount
 end
-vim.opt.foldtext = 'v:lua.myFoldText()'
+vim.opt.foldtext = 'v:lua.LmcsFoldText()'
 
 -- vim: set ts=2:sw=2:noet:sts=2:
