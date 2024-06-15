3e1ffd770e948b0b840fde0e36016c3e29fef3c8
Luis M Canaval S <public02@canaval.org>
2024-06-15 10:25:55 -0500

    lazy extras

---
+++ .config/nvim/lua/plugins/myextras.lua
@@ -0,0 +1,5 @@
+return {
+	{ "chrisbra/csv.vim", ft = "csv" },
+	{ "dylon/vim-antlr", ft = { "antlr3", "antlr4" } },
+	{ "norcalli/nvim-colorizer.lua", event = "VeryLazy" },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,41 +2,42 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"go",
-				"html",
-				"javascript",
-				"llvm",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"regex",
-				"rust",
-				"vim",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			--[[incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },]]
-			indent = {
-				enable = true,
-			},
-		},
+		-- opts = {
+		-- 	ensure_installed = {
+		-- 		"bash",
+		-- 		"c",
+		-- 		"cpp",
+		-- 		"css",
+		-- 		"go",
+		-- 		"html",
+		-- 		"javascript",
+		-- 		"llvm",
+		-- 		"lua",
+		-- 		"markdown",
+		-- 		"markdown_inline",
+		-- 		"python",
+		-- 		"regex",
+		-- 		"rust",
+		-- 		"vim",
+		-- 	},
+		-- 	sync_install = false,
+		-- 	auto_install = true,
+		-- 	highlight = {
+		-- 		enable = true,
+		-- 		additional_vim_regex_highlighting = false,
+		-- 	},
+		-- 	-- incremental_selection = {
+		-- 	-- 	enable = true,
+		-- 	-- 	keymaps = {
+		-- 	-- 		init_selection = "gnn",
+		-- 	-- 		node_incremental = "grn",
+		-- 	-- 		scope_incremental = "grc",
+		-- 	-- 		node_decremental = "grm",
+		-- 	-- 	},
+		-- 	-- },
+		-- 	indent = {
+		-- 		enable = true,
+		-- 	},
+		-- },
 	},
 }
