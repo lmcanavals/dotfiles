2cef65815f02f0d2f1d5e3445e703ff27c38a5d3
Luis M Canaval S <public02@canaval.org>
2025-11-14 22:41:41 -0500

    major zsh on rec scr

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -4,10 +4,17 @@ return {
 		---@class PluginLspOpts
 		opts = {
 			servers = {
+				bashls = {},
 				html = {},
 			},
 			setup = {
 				-- TODO: check capabilities for everything
+				bashls = function(_, opts)
+					local capabilities = vim.lsp.protocol.make_client_capabilities()
+					capabilities.textDocument.completion.completionItem.snippetSupport = true
+					opts.capabilities = capabilities
+					opts.filetypes = { "bash", "sh", "zsh" }
+				end,
 				html = function(_, opts)
 					local capabilities = vim.lsp.protocol.make_client_capabilities()
 					capabilities.textDocument.completion.completionItem.snippetSupport = true
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -22,6 +22,7 @@ return {
 				-- "rust",
 				-- "vim",
 				"wgsl",
+				"zsh",
 			},
 		},
 	},
