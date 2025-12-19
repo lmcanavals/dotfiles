c9eef1f0dcf23fc0c16e5fb13ad553960ed9f210
Luis M Canaval S <public02@canaval.org>
2025-12-19 01:36:09 -0500

    big refactor

--- .local/bin/localvolbri.zsh
+++ .local/bin/localvolbri.zsh
@@ -2,245 +2,178 @@
 
 # based on https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
 
-volume_step=5
-brightness_step=1
-max_volume=100
 notification_timeout=1000
 download_album_art=true
 show_album_art=true
 show_music_in_volume_indicator=true
 
-# TODO: when wpctl makes it easier to get this info, migrate to it
 # TODO: zsh this thing
-function get_volume {
-	pactl get-sink-volume @DEFAULT_SINK@ | rg -Po '[0-9]{1,3}(?=%)' | head -1
-}
-
-function get_mute {
-	pactl get-sink-mute @DEFAULT_SINK@ | rg -Po '(?<=Mute: )(yes|no)'
-}
-
-function get_mic_vol {
-	pactl get-source-volume @DEFAULT_SOURCE@ | rg -Po '[0-9]{1,3}(?=%)' | head -1
-}
-
-function get_mic_mute {
-	pactl get-source-mute @DEFAULT_SOURCE@ | rg -Po '(?<=Mute: )(yes|no)'
-}
-
-function get_brightness {
-	brightnessctl i | rg -Po '[0-9]{1,3}(?=%)' | head -n 1
-}
 
 function get_volume_icon {
-	volume=$(get_volume)
-	mute=$(get_mute)
-	if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ]; then
-		volume_icon=""
-	elif [ "$volume" -lt 50 ]; then
-		volume_icon=""
+	local mute=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -Po 'MUTED')
+	local icon
+	if [ "$1" -eq 0 ] || [ "$mute" == "MUTED" ]; then
+		icon=""
+	elif [ "$1" -lt 50 ]; then
+		icon=""
 	else
-		volume_icon=""
+		icon=""
 	fi
+	echo $icon
 }
 
 function get_mic_icon {
-	volume=$(get_mic_vol)
-	mute=$(get_mic_mute)
-	if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ]; then
-		volume_icon="󰍭"
+	local mute=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | rg -Po 'MUTED')
+	local icon
+	if [ "$1" -eq 0 ] || [ "$mute" == "MUTED" ]; then
+		icon="󰍭"
 	else
-		volume_icon="󰍬"
+		icon="󰍬"
 	fi
+	echo $icon
 }
 
 function get_brightness_icon {
-	brightness=$(get_brightness)
-	if [ "$brightness" -lt 11 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 22 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 33 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 44 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 55 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 66 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 77 ]; then
-		brightness_icon=""
-	elif [ "$brightness" -lt 88 ]; then
-		brightness_icon=""
-	else
-		brightness_icon=""
-	fi
-
+	local idx=$((($1 * 8 + 5) / 90))
+	local icons=("" "" "" "" "" "" "" "" "")
+	echo ${icons[$idx]}
 }
 
 function get_album_art {
-	url=$(playerctl -f "{{mpris:artUrl}}" metadata)
+	local url=$(playerctl -f "{{mpris:artUrl}}" metadata)
+	local album_art
 	if [[ $url == "file://"* ]]; then
 		album_art="${url/file:\/\//}"
-	elif [[ $url == "http://"* ]] && [[ $download_album_art == "true" ]]; then
-		filename="$(echo $url | sed "s/.*\///")"
+	elif [[ $url == "http://"* ]] ||
+		[[ $url == "https://"* ]] &&
+		[[ $download_album_art == "true" ]]; then
+		local filename="$(echo $url | sed "s/.*\///")"
 
 		if [ ! -f "/tmp/$filename" ]; then
-			wget -O "/tmp/$filename" "$url"
-		fi
-
-		album_art="/tmp/$filename"
-	elif [[ $url == "https://"* ]] && [[ $download_album_art == "true" ]]; then
-		filename="$(echo $url | sed "s/.*\///")"
-
-		if [ ! -f "/tmp/$filename" ]; then
-			wget -O "/tmp/$filename" "$url"
+			curl -o "/tmp/$filename" "$url"
 		fi
 
 		album_art="/tmp/$filename"
 	else
 		album_art="audio-speakers"
 	fi
+	echo $album_art
 }
 
 function show_volume_notif {
-	volume=$(get_mute)
-	get_volume_icon
+	local volume_icon=$(get_volume_icon $1)
 
 	if [[ $show_music_in_volume_indicator == "true" ]]; then
-		current_song=$(playerctl -f "{{title}} - {{artist}}" metadata)
+		local current_song=$(playerctl -f "{{title}}<br><b>{{artist}}</b>" metadata)
 
 		if [[ $show_album_art == "true" ]]; then
-			get_album_art
+			local album_art=$(get_album_art)
 		fi
 
-		notify-send \
-			-h string:x-dunst-stack-tag:volume_notif \
-			-h int:value:$volume \
-			-i "$album_art" \
-			-t $notification_timeout \
-			"$volume% $volume_icon" \
-			"$current_song"
+		fyi --hint=string:x-dunst-stack-tag:volume_notif --hint=int:value:$1 \
+			-i "$album_art" -t $notification_timeout "$1% $volume_icon" "$current_song"
 	else
-		notify-send \
-			-h string:x-dunst-stack-tag:volume_notif \
-			-h int:value:$volume \
-			-i audio-speakers \
-			-t $notification_timeout \
-			"$volume% $volume_icon"
+		fyi --hint=string:x-dunst-stack-tag:volume_notif --hint=int:value:$1 \
+			-i audio-speakers -t $notification_timeout "$1% $volume_icon"
 	fi
 }
 
 function show_mic_notif {
-	volume=$(get_mic_mute)
-	get_mic_icon
-	notify-send \
-		-h string:x-dunst-stack-tag:mic_notif \
-		-h int:value:$volume \
-		-i audio-input-microphone \
-		-t $notification_timeout \
-		"$volume% $volume_icon"
+	local mic_icon=$(get_mic_icon $1)
+	fyi --hint=string:x-dunst-stack-tag:mic_notif --hint=int:value:$1 \
+		-i audio-input-microphone -t $notification_timeout "$1% $mic_icon"
 }
 
 function show_music_notif {
-	song_title=$(playerctl -f "{{title}}" metadata)
-	song_artist=$(playerctl -f "{{artist}}" metadata)
-	song_album=$(playerctl -f "{{album}}" metadata)
+	local song_title=$(playerctl -f "{{title}}" metadata)
+	local song_artist=$(playerctl -f "{{artist}}" metadata)
+	local song_album=$(playerctl -f "{{album}}" metadata)
+	local album_art="music"
 
 	if [[ $show_album_art == "true" ]]; then
-		get_album_art
+		local album_art=$(get_album_art)
+	fi
+	if [[ -n "$song_album" ]]; then
+		song_album="<br><b>song_album</b>"
 	fi
 
-	notify-send \
-		-h string:x-dunst-stack-tag:music_notif \
-		-i "$album_art" \
-		-t $notification_timeout \
-		"$song_title" \
-		"$song_artist - $song_album"
+	fyi --hint=string:x-dunst-stack-tag:music_notif \
+		-i "$album_art" -t $notification_timeout "$song_title" "$song_artist$song_album"
 }
 
 function show_brightness_notif {
-	brightness=$(get_brightness)
-	echo $brightness
-	get_brightness_icon
-	notify-send \
-		-h string:x-dunst-stack-tag:brightness_notif \
-		-h int:value:$brightness \
-		-i display \
-		-t $notification_timeout \
-		"$brightness% $brightness_icon"
+	local brightness=$(brightnessctl i | rg -Po '[0-9]{1,3}(?=%)' | head -n 1)
+	local brightness_icon=$(get_brightness_icon $brightness)
+	fyi --hint=string:x-dunst-stack-tag:brightness_notif --hint=int:value:$brightness \
+		-i display -t $notification_timeout "$brightness% $brightness_icon"
 }
 
-case $1 in
-volume_up)
-	wpctl set-mute @DEFAULT_SINK@ 0
-	volume=$(get_volume)
-	if [ $(("$volume" + "$volume_step")) -gt $max_volume ]; then
-		wpctl set-volume @DEFAULT_SINK@ $max_volume%
-	else
-		wpctl set-volume @DEFAULT_SINK@ $volume_step%+
-	fi
-	show_volume_notif
-	;;
+function volbri() {
+	local volume_step=5
+	local brightness_step=1
+	local max_volume=100
 
-volume_down)
-	wpctl set-volume @DEFAULT_SINK@ $volume_step%-
-	show_volume_notif
-	;;
+	# get audio device
+	case $1 in
+	mic_down | mic_mute | mic_up) local device="@DEFAULT_SOURCE@" ;;
+	volume_down | volume_mute | volume_up) local device="@DEFAULT_SINK@" ;;
+	esac
 
-volume_mute)
-	wpctl set-mute @DEFAULT_SINK@ toggle
-	show_volume_notif
-	;;
+	# get and update volume for device
+	case $1 in
+	mic_down | mic_mute | mic_up | volume_down | volume_mute | volume_up)
+		local volume=$(wpctl get-volume $device | awk '{printf "%.0f\n", $2 * 100}')
+		;;
+	esac
 
-mic_up)
-	wpctl set-mute @DEFAULT_SOURCE@ 0
-	volume=$(get_mic_vol)
-	if [ $(("$volume" + "$volume_step")) -gt $max_volume ]; then
-		wpctl set-volume @DEFAULT_SOURCE@ $max_volume%
-	else
-		wpctl set-volume @DEFAULT_SOURCE@ $volume_step%+
-	fi
-	show_mic_notif
-	;;
+	# calculate new volume level
+	case $1 in
+	mic_down | volume_down) volume=$((volume - volume_step)) ;;
+	mic_up | volume_up)
+		volume=$((volume + volume_step))
+		if [ $volume -gt $max_volume ]; then
+			volume=$max_volume
+		fi
+		;;
+	esac
 
-mic_down)
-	wpctl set-volume @DEFAULT_SOURCE@ $volume_step%-
-	show_mic_notif
-	;;
+	# update values
+	case $1 in
+	brightness_down) brightnessctl set $brightness_step%- ;;
+	brightness_up) brightnessctl set $brightness_step%+ ;;
+	mic_down | mic_up | volume_down | volume_up) wpctl set-volume $device $volume% ;;
+	mic_mute | volume_mute) wpctl set-mute $device toggle ;;
+	esac
 
-mic_mute)
-	wpctl set-mute @DEFAULT_SOURCE@ toggle
-	show_mic_notif
-	;;
+	# showing notifs
+	case $1 in
+	mic_down | mic_mute | mic_up) show_mic_notif $volume ;;
+	volume_down | volume_mute | volume_up) show_volume_notif $volume ;;
+	brightness_down | brightness_up) show_brightness_notif ;;
+	esac
 
-brightness_up)
-	brightnessctl set $brightness_step%+
-	show_brightness_notif
-	;;
+	# media player control
+	case $1 in
+	next_track)
+		playerctl next
+		sleep 0.5 && show_music_notif
+		;;
 
-brightness_down)
-	brightnessctl set $brightness_step%-
-	show_brightness_notif
-	;;
+	prev_track)
+		playerctl previous
+		sleep 0.5 && show_music_notif
+		;;
 
-next_track)
-	playerctl next
-	sleep 0.5 && show_music_notif
-	;;
+	play_pause)
+		playerctl play-pause
+		show_music_notif
+		;;
 
-prev_track)
-	playerctl previous
-	sleep 0.5 && show_music_notif
-	;;
+	play_stop)
+		show_music_notif
+		playerctl stop
+		;;
+	esac
+}
 
-play_pause)
-	playerctl play-pause
-	show_music_notif
-	;;
-
-play_stop)
-	show_music_notif
-	playerctl stop
-	;;
-esac
+volbri $@
