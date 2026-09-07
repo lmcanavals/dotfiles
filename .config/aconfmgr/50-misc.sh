cb64316b2853c1a4ea0ca5e5b113e59e8d74bcf2
Luis M Canaval S <public02@canaval.org>
2026-09-07 00:06:18 -0500

    clean up

--- .config/aconfmgr/50-misc.sh
+++ .config/aconfmgr/50-misc.sh
@@ -2,16 +2,10 @@ AddPackage 7zip
 AddPackage bat
 AddPackage btop
 AddPackage chafa
-AddPackage cups
 AddPackage dust
 AddPackage eza
 AddPackage fd
-AddPackage font-manager
-AddPackage fyi
 AddPackage fzf
-AddPackage gphoto2
-AddPackage gvfs
-AddPackage gvfs-gphoto2
 AddPackage imagemagick
 AddPackage jq
 AddPackage less
@@ -21,32 +15,21 @@ AddPackage mc
 AddPackage mosh
 AddPackage ntfs-3g
 AddPackage procs
-AddPackage qalculate-qt
 AddPackage ripgrep
 AddPackage sd
-AddPackage showmethekey
-AddPackage speech-dispatcher
 AddPackage speedtest-cli
 AddPackage spirv-tools
 AddPackage starship
-AddPackage swappy
 AddPackage tectonic
 AddPackage terminus-font
 AddPackage unarchiver
 AddPackage unrar
-AddPackage v4l2loopback-dkms
-AddPackage zenity
 
 # This are needed for https://github.com/CyberShadow/aconfmgr
 AddPackage expect
 AddPackage pacutils
 
-AddPackage --foreign hyprqt6engine
-AddPackage --foreign maplemono-ttf
 AddPackage --foreign psf-cozette
-AddPackage --foreign rose-pine-hyprcursor
-AddPackage --foreign ttf-homecomputer-sixtyfour
-AddPackage --foreign ttf-jura
 AddPackage --foreign yay
 
 # packages we should ignore
@@ -64,7 +47,6 @@ IgnorePackage go
 IgnorePackage graphviz
 IgnorePackage gthumb
 IgnorePackage guvcview
-IgnorePackage intel-ucode
 IgnorePackage krita
 IgnorePackage lld
 IgnorePackage lldb
@@ -85,6 +67,3 @@ IgnorePackage wl-mirror
 IgnorePackage wofi
 IgnorePackage xorg-server
 IgnorePackage xournalpp
-
-# here some packages that are optional form others but might not be that important?
-# lvm2 ddcutil libopenraw glib2-devel foomatic-db-gutenprint-ppds meson btrfs-progs usbmuxd geoclue
