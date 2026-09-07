713dd3e537a3e30a098480586e34fb8eb8e13963
Luis M Canaval S <public02@canaval.org>
2026-09-07 17:27:19 -0500

    xdg :(

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
+# XDG essentials
+set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME "$HOME/.local/state"
+
 # Clang stuff
 set -gx CC /usr/bin/clang
 set -gx CXX '/usr/bin/clang++'
